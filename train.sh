6c637ed41b678d671010589632e5ca32b5ff8fef
kureta <skureta@gmail.com>
2022-05-24 12:41:05 +0300

    prepare for training in the cloud #8

--- train.sh
+++ train.sh
@@ -2,5 +2,5 @@
 apt update && apt install libsndfile1 -y
 pip install -r cloud.txt
 
-tensorboard --logdir /workspace/container_1/lightning_logs --bind_all --port 80 &
+tensorboard --logdir /workspace/container_1/lightning_logs --host 0.0.0.0 --port 80 &
 python main.py fit --trainer.gpus 1 --trainer.max_epochs=-1 --trainer.default_root_dir=/workspace/container_1/ --model.lr=0.0003 --data.batch_size=16 --data.path=/workspace/container_1/features.pth
