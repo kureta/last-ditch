36c9ec597327c8a56593095300d5d9310811f1ba
kureta <skureta@gmail.com>
2022-05-24 11:45:42 +0300

    prepare for training in the cloud #5

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 tensorboard --logdir /workspace/container_1/lightning_logs --bind_all --port 80 &
-python main.py fit --trainer.gpus 1 --trainer.max_epochs=-1 --trainer.default_root_dir=/workspace/container_1/ --model.lr=0.0003 --data.batch_size=16 --data.path=/workspace/container_1/features.pth
+python main.py fit --trainer.gpus 0 --trainer.max_epochs=-1 --trainer.default_root_dir=/workspace/container_1/ --model.lr=0.0003 --data.batch_size=16 --data.path=/workspace/container_1/features.pth
